088251838c37fc67af3de17bc116d6ed68d48b67
TASOS53 <79927851+TASOS53@users.noreply.github.com>
2021-03-02 19:40:30 +0200

    Add files via upload

--- exercise3.py
+++ exercise3.py
@@ -6,11 +6,13 @@ import time
 DrawIdList=[]
 current_date=datetime.datetime.now()
 current_day=current_date.strftime("%d")
+y=current_date.strftime("%Y-%m-")
+
 for i in range (1, int(current_day)+1):
     if i<10:
-        FromDate="2021-02-0" +  str(i)
+        FromDate=y+ "0" +  str(i)
     else:
-        FromDate="2021-02-" + str(i)
+        FromDate=y + str(i)
     ToDate=FromDate
     url="https://api.opap.gr/draws/v3.0/1100/draw-date/" + FromDate + "/" + ToDate +'/draw-id'
     response=urllib.request.urlopen(url)
@@ -37,8 +39,6 @@ for i in range(80):
 for i in range(int(current_day)):
     for j in range(20):
         array[winningNumbersList[i][j]-1]+=1
-print(array)
-print("\n")
 
 for i in range(80):
     print("Νούμερο:%d Ποσοστό εμφάνισης:%d" %(i+1, (array[i]/int(current_day))*100))
--- exercise9.py
+++ exercise9.py
@@ -10,12 +10,11 @@ for i in range(1,127,2):
 totalCharCount=0
 for line in file:
     for charachter in line:
-        asciiNumber=ord(charachter)
-        if asciiNumber%2==1:
+        x=ord(charachter)
+        if x%2==1:
             totalCharCount+=1
-            totalValueOfEachChar=dictionary[charachter]+1
+            y=dictionary[charachter]+1
             dictionary.update({charachter:y})
-            print(dictionary)
 
 for dictKey, dictValue in dictionary.items():
     percentage = math.ceil((dictValue / totalCharCount) * 100)
